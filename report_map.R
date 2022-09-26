43a5f1b38cff68559a211aeb18bc1847a34f0192
Arni Magnusson <thisisarni@gmail.com>
2022-09-26 16:04:56 +1100

    Plot map with green countries

--- report_map.R
+++ report_map.R
@@ -22,11 +22,14 @@ world$group <- as.integer(world$group)
 rownames(world) <- NULL
 world2 <- rbind(world, transform(world, long=long-360, group=group+10000L))
 
-## Plot (no projection)
+## Plot map
 pdf("report/map.pdf", width=12, height=6)
 plot(NA, xlim=c(-251,165), ylim=c(-50,68), xlab="Lon", ylab="Lat")
-map <- lapply(split(world2, world2$group), polygon)
-with(flights, segments(Efrom, Nfrom, Eto, Nto))
+map <- lapply(split(world2, world2$group), polygon, border="white", lwd=0.4,
+              col="olivedrab4")
+with(flights, segments(Efrom, Nfrom, Eto, Nto, lwd=1.5))
 points(Latitude~Longitude, cities, subset=Overnight, pch=16, cex=2, col="red")
-points(Latitude~Longitude, cities, subset=!Overnight, pch=16, cex=2, col="pink")
+points(Latitude~Longitude, cities, subset=!Overnight, pch=16, cex=2,
+       col="darkgray")
+box()
 dev.off()
