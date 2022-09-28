9a255ebde5a29108b00c40e0b503941e3565845f
Arni Magnusson <thisisarni@gmail.com>
2022-09-28 23:51:14 +1100

    Use taf2html() from develop version of TAF

--- report.R
+++ report.R
@@ -1,4 +1,7 @@
 library(TAF)
+library(rmarkdown)
 
 sourceTAF("report_map.R")
 sourceTAF("report_tables.R")
+render("report.Rmd")
+sourceTAF("report_www.R")
---
+++ report_www.R
@@ -0,0 +1,26 @@
+## Make map and tables available in www folder
+
+## Before: template.html, trip.css (bootstrap/software),
+#          cities.csv, flights.csv, map.svg (report)
+## After:  index.html (root), map.svg, trip.css (www)
+
+library(TAF)
+
+mkdir("www")
+
+## Read tables
+cities <- read.taf("report/cities.csv")
+flights <- read.taf("report/flights.csv")
+
+## Copy files
+cp("bootstrap/software/trip.css", "www")
+cp("bootstrap/software/template.html", "index.html")
+cp("report/map.svg", "www")
+
+## Add tables and link
+tab.flights <- taf2html(flights, file="index.html", append=TRUE,
+                        align=c("R","R","L","L","L","L","R"))
+tab.cities <- taf2html(cities, file="index.html", append=TRUE,
+                       align=c("R","L","L"))
+bottom <- '<p><a href="https://github.com/arni-magnusson/trip">source</a>\n'
+cat(bottom, file="index.html", append=TRUE)
